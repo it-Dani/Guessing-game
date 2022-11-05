a5645ac9575e135466057355b3d0bf2c4bef1438
Danial Ahmadi <105815120+it-Dani@users.noreply.github.com>
2022-11-05 14:09:10 +0330

    Add files via upload

---
+++ Guessing Game/myscript.js
@@ -0,0 +1,41 @@
+let ranNumber = Math.floor(Math.random()*20);
+function game() {
+    let geuss = document.getElementById("num-inp").value;
+    let txt = document.querySelector("#historyarea > h3");
+    let point = document.getElementById("chance-point").innerHTML = Number(document.getElementById("chance-point").innerHTML)-1;
+
+    if (geuss && geuss >= 1) {
+        if (geuss > ranNumber) {
+            txt.innerHTML = "بزرگه عددت";
+        } else if(geuss < ranNumber) {
+            txt.innerHTML = "کوچیکه عددت";
+        }else{
+            txt.innerHTML = ":) بردی ";
+            document.getElementById("num-inp").disabled = true;
+            document.getElementById("chance-txt").innerHTML = " بردی تبریک میگم :) ";
+            document.getElementById("chance-point").style.display ="none";
+            document.getElementById("btn").disabled = true;
+            document.getElementById("main-box").style.backgroundColor="green";
+
+        }
+    }
+    if (geuss > 20) {
+        txt.innerHTML ="عدد شما از 20 بزرگتر است و الکی یه جونت رفت"
+    }
+    if (geuss <= 0) {
+        txt.innerHTML =":( عددت از یک کوچیکتره الکی یه جونت پرید "
+    }
+    if (geuss == null || geuss == "") {
+        txt.innerHTML ="عددی که وارد نکردی"
+    }
+    if(point == 0){
+        txt.innerHTML = "باختی";
+        document.getElementById("num-inp").disabled = true;
+        document.getElementById("chance-txt").innerHTML = "دوباره شانستو امتحان کن";
+        document.getElementById("chance-point").style.display ="none";
+        document.getElementById("btn").disabled = true;
+        document.getElementById("main-box").style.backgroundColor="red";
+
+    }
+}
+
